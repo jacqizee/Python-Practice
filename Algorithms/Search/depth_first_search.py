ef911bc8eb576cdf93a6d60f6ef59d653c919e11
Jacqueline <jhjzhou@gmail.com>
2022-08-08 10:53:24 -0400

    adding graph DFS

--- Algorithms/Search/depth_first_search.py
+++ Algorithms/Search/depth_first_search.py
@@ -1,5 +1,17 @@
-# DFS can be achieved with the help of a stack or recursion
+# DFS can be achieved with the help of either a stack or recursion
 
+# Recursive Method - more common
+def dfs(graph, current_vertex, target_vertex, visited=None):
+    if visited is None:
+        visited = []
 
-def dfs(graph, current_vertex, target_vertex, visited):
-    pass
+    visited.append(current_vertex)
+
+    if current_vertex == target_vertex:
+        return visited
+
+    for adjacent_vertex in graph[current_vertex]:
+        if adjacent_vertex not in visited:
+            path = dfs(adjacent_vertex)
+        if path:
+            return path
