3856cec417bb714863721ccbdf94d0612187ab3e
Jacqueline <jhjzhou@gmail.com>
2022-08-11 10:32:25 -0400

    adding test for quick sort:

--- Algorithms/Sort/testing.py
+++ Algorithms/Sort/testing.py
@@ -1,4 +1,5 @@
 import unittest
+from quick_sort import quick_sort
 from bubble_sort import bubble_sort
 from merge_sort import merge_sort
 from selection_sort import selection_sort
@@ -28,6 +29,12 @@ class TestSortingAlgos(unittest.TestCase):
         self.assertEqual(insertion_sort([3, 2]), [2, 3])
         self.assertEqual(insertion_sort([1, 2]), [1, 2])
         self.assertEqual(insertion_sort([5, 4, 4, 3, 2, 1]), [1, 2, 3, 4, 4, 5])
+
+    def test_quick(self):
+        print('\nTesting Quick Sort')
+        array = [5, 4, 4, 3, 2, 1]
+        quick_sort(array, 0, len(array) - 1)
+        self.assertEqual(array, [1, 2, 3, 4, 4, 5])
 
 if __name__ == '__main__':
     unittest.main()
