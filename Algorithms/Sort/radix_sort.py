35c7c8a2bd103802bd2a2f002caf95c9efb6b13d
Jacqueline <jhjzhou@gmail.com>
2022-08-11 11:03:00 -0400

    adding radix sort and test for radix

--- Algorithms/Sort/radix_sort.py
+++ Algorithms/Sort/radix_sort.py
@@ -1,2 +1,21 @@
-def radix_sort(arr):
-    pass
+# Radix sorts by grouping numbers by digits place, rather than comparing numbers against one another
+# aka a non-comparison sort
+
+def radix_sort(arr: 'list[int]'):
+    digits = len(str(max(arr)))
+    being_sorted = arr[:]
+
+    for i in range(digits):
+        holder = [[] for digit in range(10)]
+        index = -(i + 1)
+        for num in being_sorted:
+            num_as_str = str(num)
+            try:
+                holder[int(num_as_str[index])].append(num)
+            except:
+                holder[0].append(num)
+        being_sorted = []
+        for digits_place in holder:
+            being_sorted += digits_place
+    
+    return being_sorted
--- Algorithms/Sort/testing.py
+++ Algorithms/Sort/testing.py
@@ -1,4 +1,5 @@
 import unittest
+from radix_sort import radix_sort
 from quick_sort import quick_sort
 from bubble_sort import bubble_sort
 from merge_sort import merge_sort
@@ -9,25 +10,25 @@ class TestSortingAlgos(unittest.TestCase):
     def test_bubble(self):
         print('Testing Bubble Sort')
         self.assertEqual(bubble_sort([3, 2]), [2, 3])
-        self.assertEqual(bubble_sort([1, 2]), [1, 2])
+        self.assertEqual(bubble_sort([5, 532, 5, 252, 56, 653, 876, 89, 1, 4]), [1, 4, 5, 5, 56, 89, 252, 532, 653, 876])
         self.assertEqual(bubble_sort([5, 4, 4, 3, 2, 1]), [1, 2, 3, 4, 4, 5])
 
     def test_merge(self):
         print('\nTesting Merge Sort')
         self.assertEqual(merge_sort([3, 2]), [2, 3])
-        self.assertEqual(merge_sort([1, 2]), [1, 2])
+        self.assertEqual(merge_sort([5, 532, 5, 252, 56, 653, 876, 89, 1, 4]), [1, 4, 5, 5, 56, 89, 252, 532, 653, 876])
         self.assertEqual(merge_sort([5, 4, 4, 3, 2, 1]), [1, 2, 3, 4, 4, 5])
 
     def test_selection(self):
         print('\nTesting Selection Sort')
         self.assertEqual(selection_sort([3, 2]), [2, 3])
-        self.assertEqual(selection_sort([1, 2]), [1, 2])
+        self.assertEqual(selection_sort([5, 532, 5, 252, 56, 653, 876, 89, 1, 4]), [1, 4, 5, 5, 56, 89, 252, 532, 653, 876])
         self.assertEqual(selection_sort([5, 4, 4, 3, 2, 1]), [1, 2, 3, 4, 4, 5])
     
     def test_insertion(self):
         print('\nTesting Selection Sort')
         self.assertEqual(insertion_sort([3, 2]), [2, 3])
-        self.assertEqual(insertion_sort([1, 2]), [1, 2])
+        self.assertEqual(insertion_sort([5, 532, 5, 252, 56, 653, 876, 89, 1, 4]), [1, 4, 5, 5, 56, 89, 252, 532, 653, 876])
         self.assertEqual(insertion_sort([5, 4, 4, 3, 2, 1]), [1, 2, 3, 4, 4, 5])
 
     def test_quick(self):
@@ -35,6 +36,12 @@ class TestSortingAlgos(unittest.TestCase):
         array = [5, 4, 4, 3, 2, 1]
         quick_sort(array, 0, len(array) - 1)
         self.assertEqual(array, [1, 2, 3, 4, 4, 5])
+    
+    def test_radix(self):
+        print('\nTesting Radix Sort')
+        self.assertEqual(radix_sort([3, 2]), [2, 3])
+        self.assertEqual(radix_sort([5, 532, 5, 252, 56, 653, 876, 89, 1, 4]), [1, 4, 5, 5, 56, 89, 252, 532, 653, 876])
+        self.assertEqual(radix_sort([5, 4, 4, 3, 2, 1]), [1, 2, 3, 4, 4, 5])
 
 if __name__ == '__main__':
     unittest.main()
